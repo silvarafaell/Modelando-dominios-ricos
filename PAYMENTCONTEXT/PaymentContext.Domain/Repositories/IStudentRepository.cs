935ca630487b26b49764342758518e1e4850db2c
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-03-20 22:24:42 -0300

    Repository Pattern

---
+++ PAYMENTCONTEXT/PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+using PAYMENTCONTEXT.Domain.Entities;
+
+namespace PaymentContext.Domain.Repositories
+{
+    public interface IStudentRepository
+    {
+        bool DocumentExists(string document);
+        bool EmailExists(string email);
+        void CreateSubscription(Student student);
+    }
+}
